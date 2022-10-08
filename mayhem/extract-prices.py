a62f61c4557cb11c07be3d83cde626751963f245
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-08 15:36:49 -0400

    number fix

--- mayhem/extract-prices.py
+++ mayhem/extract-prices.py
@@ -5,24 +5,22 @@ import sys
 with atheris.instrument_imports():
     from price_parser import Price
 
-
-
 @atheris.instrument_func
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
     num = fdp.ConsumeIntInRange(0, 2)     
 
-    if num == 1:
+    if num == 0:
         in_str_1 = fdp.ConsumeUnicodeNoSurrogates(64)
         in_str_2 = fdp.ConsumeUnicodeNoSurrogates(64)
         price = Price.fromstring(in_str_1, currency_hint=in_str_2)
 
-    if num == 2:
+    if num == 1:
         in_str_1 = fdp.ConsumeUnicodeNoSurrogates(64)
         in_str_2 = fdp.ConsumeUnicodeNoSurrogates(64)
         price = Price.fromstring(in_str_1, decimal_separator=in_str_2)
 
-    if num == 3:
+    if num == 2:
         in_str_1 = fdp.ConsumeUnicodeNoSurrogates(64)
         in_str_2 = fdp.ConsumeUnicodeNoSurrogates(64)
         in_str_3 = fdp.ConsumeUnicodeNoSurrogates(64)
