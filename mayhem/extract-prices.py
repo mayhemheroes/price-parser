9135abd68375bbaa55b7ed18398f8a4b64df2f9a
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-08 14:12:36 -0400

    better handling

--- mayhem/extract-prices.py
+++ mayhem/extract-prices.py
@@ -6,11 +6,7 @@ with atheris.instrument_imports():
     from price_parser import Price
 
 @atheris.instrument_func
-def TestOneInput(data):
-    fdp = atheris.FuzzedDataProvider(data)
-    in_str = fdp.ConsumeString(128)
-    price = Price.fromstring(in_str)
-
+def ValidateResult(price):
     if price.amount == None:
         return
 
@@ -19,6 +15,23 @@ def TestOneInput(data):
 
     assert(type(price.currency) == str, 'currency should be a string')
     assert(type(price.amount) == float, 'currency should be a float')
+
+@atheris.instrument_func
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+    num = fdp.ConsumeIntInRange(0, 2)
+    in_str_1 = fdp.ConsumeUnicodeNoSurrogates(64)
+    in_str_2 = fdp.ConsumeUnicodeNoSurrogates(64)
+    in_str_3 = fdp.ConsumeUnicodeNoSurrogates(64)
+
+    if num == 0:
+        ValidateResult(Price.fromstring(in_str_1))
+
+    if num == 1:
+        ValidateResult(Price.fromstring(in_str_1, in_str_2))
+
+    if num == 2:
+        ValidateResult(Price.fromstring(in_str_3, in_str_2, in_str_3))
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
